8a20adf9d20f66e2e879b8142a13f54b4476d5e4
MilanSas <milansas1@gmail.com>
2017-01-17 13:17:13 +0100

    colors function for python
    import this file and you can use the color function

---
+++ colors.py
@@ -0,0 +1,28 @@
+# RGB Color Definitions
+# By: Wesley van Schaijk
+# Designed for pygame
+
+def black(): return 0, 0, 0
+def white(): return 255, 255, 255
+def red(): return 255, 0, 0
+def dark_red(): return 135, 30, 30
+def light_red(): return 230, 50, 50
+def green(): return 0, 255, 0
+def dark_green(): return 30, 135, 30
+def light_green(): return 125, 240, 125
+def blue(): return 0, 0, 255
+def dark_blue(): return 30, 30, 135
+def light_blue(): return 110, 160, 255
+def bluegreen(): return 20, 230, 195
+def greenyellow(): return 185, 230, 21
+def orange(): return 255, 150, 0
+def pink(): return 255, 163, 163
+def dark_pink(): return 235, 25, 135
+def zalm_pink(): return 225, 190, 205
+def light_purple(): return 190, 45, 245
+def purple(): return 140, 30, 185
+def grey(): return 180, 180, 180
+def light_grey(): return 200, 200, 200
+def dark_grey(): return 100, 100, 100
+def yellow(): return 255, 255, 0
+def brown(): return 110, 80, 16
