3ceed75174dd058c5d92acb8cfa269166ed9f536
Ino van den Berg <0928787@hr.nl>
2017-01-18 10:33:50 +0100

    Start of game menu
    see title

--- BoardGame.py
+++ BoardGame.py
@@ -1,6 +1,22 @@
 import pygame
-traversablelist = [(9, 9), (4, 3), (2, 5), (6,5), (3,5), (29, 19), (15,10)]
-policesquares = [(7,8), (3, 6)]
+
+#colors
+red = (255, 0, 0)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+brown = (110, 60, 19)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+
+
+#lists
+traversablelist = [(2, 0)]
+line = [(16,0), (16,1), (16,2), (16,3), (16, 6), (16,7), (16,8), (16,9), (16,10), (16,11), (16,12), (16,13), (16,16), (16,19)]
+landmarks = [(4,0), (8,2), (7,17), (11,8), (12,14), (14,19), (27,19), (29,16), (31,3), (25,4), (26,8), (21,0), (14,3), (23,15), (19,11), (16,11), (19,4), (29,11)]
+policesquares = [(7,6), (10,15), (16,0), (20,17), (28,4), (19,10)]
+arenas = [(5,19), (6,19), (7,19), (8,19), (9,19), (7,18), (10,11), (11,11), (12,11), (12,12), (15,13), (16,13), (15,14), (15,15), (15,16), (16,16), (10,2), (11,2), (11,3), (11,4), (11,5), (11,6), (10,6), (15,6), (16,6), (17,6), (18,6), (19,6), (20,6), (20,5), (15,5)]
+kanskaarten = [(2,5), (2,16), (4,9), (7,7), (7,13), (7,19), (9,17), (10,2), (10,6), (12,0), (12,4), (12,13), (13,8), (15,0), (15,16), (16,8), (17,3), (18,13), (19,6), (20,19), (22,8), (23,6), (23,13), (26,0), (26,6), (26,11), (27,16), (31,1), (31,9), (31,16)]
+
 class Vector2:
     def __init__(self, x, y):
         self.x = x
@@ -8,7 +24,6 @@ class Vector2:
 
     def is_same(self, v2):
         return self.x == v2.x and self.y == v2.y
-
 
 class Tile:
     def __init__(self, pos: Vector2, tail, traversable, type):
@@ -18,9 +33,17 @@ class Tile:
         self.traversable = traversable
     def draw(self, screen, cSize, rSize):
         if self.traversable:
-            pygame.draw.rect(screen, (255, 255, 255), [self.pos.x * cSize, self.pos.y * rSize, cSize - 2, rSize - 2])
+            pygame.draw.rect(screen, white, [self.pos.x * cSize, self.pos.y * rSize, cSize - 2, rSize - 2])
+        if self.type == "Line":
+            pygame.draw.rect(screen, yellow, [self.pos.x * cSize, self.pos.y * rSize, cSize - 2, rSize - 2])
+        if self.type == "Kans":
+            pygame.draw.rect(screen, green, [self.pos.x * cSize, self.pos.y * rSize, cSize - 2, rSize - 2])
         if self.type == "Police":
-            pygame.draw.rect(screen, (0, 0, 255), [self.pos.x * cSize, self.pos.y * rSize, cSize - 2, rSize - 2])
+            pygame.draw.rect(screen, blue, [self.pos.x * cSize, self.pos.y * rSize, cSize - 2, rSize - 2])
+        if self.type == "Arena":
+            pygame.draw.rect(screen, red, [self.pos.x * cSize, self.pos.y * rSize, cSize -2, rSize - 2])
+        if self.type == "Landmark":
+            pygame.draw.rect(screen, brown, [self.pos.x * cSize, self.pos.y * rSize, cSize -2, rSize - 2])
         if self.tail is not None:
             self.tail.draw(screen, cSize, rSize)
 
@@ -39,8 +62,16 @@ class Game:
             for x in range(0, self.colloms):
                 if (x, y) in policesquares:
                     board = Tile(Vector2(x, y), board, True, "Police")
+                if (x, y) in arenas:
+                    board = Tile(Vector2(x, y), board, True, "Arena")
+                if (x, y) in line:
+                    board = Tile(Vector2(x, y), board, True, "Line")
                 if (x, y) in traversablelist:
                     board = Tile(Vector2(x, y), board, True, "Tile")
+                if (x, y) in landmarks:
+                    board = Tile(Vector2(x, y), board, True, "Landmark")
+                if (x, y) in kanskaarten:
+                    board = Tile(Vector2(x, y), board, True, "Kans")
                 else:
                     board = Tile(Vector2(x, y), board, False, "Empty")
         return board
@@ -50,4 +81,3 @@ class Game:
         rSize = self.height // self.rows
 
         self.board.draw(screen, cSize, rSize)
-
--- main.py
+++ main.py
@@ -1,24 +1,64 @@
 import pygame
 import sys
+from time import sleep
 from BoardGame import Game
 pygame.init()
 
-size = width, height = 1366 , 768
-grid = colloms, rows = 30, 20
+#colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+size = width, height = (1280, 720)
+grid = colloms, rows = 32, 20
 clock = pygame.time.Clock()
-black = 0, 0, 0
+basicfont = pygame.font.Font('freesansbold.ttf', 18)
+displaysurf = pygame.display.set_mode((width, height))
 screen = pygame.display.set_mode(size)
 game = Game(colloms,rows,width,height)
 
-while True:
-    events = pygame.event.get()
+events = pygame.event.get()
+key = pygame.key.get_pressed()
+menu = True
 
+def printText(text, font, color):
+    area = font.render(text, True, color)
+    return area, area.get_rect()
+
+while menu:
     for event in events:
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT or key[pygame.K_ESCAPE]:
+            exit()
+        elif key[pygame.K_KP_ENTER]:
+            menu = False
+            game_loop()
+    screen.fill(white)
+    title = pygame.font.Font('freesansbold.ttf', 50)
+    sub = pygame.font.Font('freesansbold.ttf', 25)
+    titleText, titleRect = printText("Ontsnapperdam", title, black)
+    titleRect.center = ((width/2), (50))
+    screen.blit(titleText, titleRect)
+    startText, startRect = printText("Start game", sub, black)
+    startRect.center = ((width/2), (height/3))
+    screen.blit(startText, startRect)
+    clock.tick(15)
+    pygame.display.update()
 
-    screen.fill(black)
+def game_loop():
+    while not menu:
+        for event in events:
+            if event.type == pygame.QUIT or key[pygame.K_ESCAPE]:
+                screen.fill(red)
+                endText = basicfont.render('Game over!', True, white)
+                endRect = endText.get_rect()
+                endRect.topleft = (10, 10)
+                displaysurf.blit(endText, endRect)
+                pygame.display.flip()
+                sleep(1)
+                exit()
 
-    game.draw(screen)
+        screen.fill(black)
 
-    pygame.display.flip()
+        game.draw(screen)
 
+        pygame.display.flip()
