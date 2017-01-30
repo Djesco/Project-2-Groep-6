1cfaedc791b572c6aa04ea08d50d06b0fbc66f22
nikkichi <0930364@hr.nl>
2017-01-30 13:25:18 +0100

    Database
    het werkt alleen nog variables fixen eyyyyyyyyy

--- database.py
+++ database.py
@@ -1,40 +1,44 @@
 import psycopg2
 
-# Connect and set up cursor
 def interaction_database(command):
     connection = psycopg2.connect("dbname=Ontsnapperdam user=postgres host=localhost port=5432")
     cursor = connection.cursor()
 
-# Execute command
     cursor.execute(command)
     connection.commit()
 
-# Save results
     results = None
     try:
         results = cursor.fetchall()
     except psycopg2.ProgrammingError:
-# Nothing to fetch
         pass
 
-# Close connection
     cursor.close()
     connection.close()
 
     return results
 
-# Uploads a score into the highscore table
-def upload_score(, score):
-    interaction_database("UPDATE highscores SET highscores =  WHERE usernames = '{}'"
-                         .format(score, name))
+def upload_score(name, score):
+    interaction_database("UPDATE highscores SET highscores = {} WHERE usernames = '{}'"
+                           .format(score, name))
+
+def insert_players(name, score):
+    interaction_database("INSERT INTO highscores VALUES ('{}', NULL)". format(name, score))
 
 
-# Downloads score data from database
 def download_scores():
     return interaction_database("SELECT * FROM highscores")
 
-
-# Downloads the top score from database
 def download_top_score():
-    result = interaction_database("SELECT * FROM highscores ORDER BY highscores")[0][1]
+    result = ""
+    result += interaction_database("SELECT * FROM highscores ORDER BY highscores ASC")[0][0] + " "
+    result += str(interaction_database("SELECT * FROM highscores ORDER BY highscores ASC")[0][1])
     return result
+
+allscores = download_scores()
+winner = download_top_score()
+
+print(download_scores())
+
+print(download_top_score())
+
