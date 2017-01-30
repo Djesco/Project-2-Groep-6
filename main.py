ffa0995688f25dd66584183b7b2d9adcbbb7b735
nikkichi <0930364@hr.nl>
2017-01-30 14:46:22 +0100

    main
    fullscreen gewijzigd

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import colors
 from Game import Game
 from main_menu import Main_menu 
 pygame.init()
-
-# fullscreen = pygame.FULLSCREEN
+import Board as b
+fullscreen = pygame.FULLSCREEN
 width = 1280
 height = 720
-screen = pygame.display.set_mode((width,height))#, fullscreen)
+screen = pygame.display.set_mode((width,height), (fullscreen))
 
 pygame.display.set_caption("Ontsnapperdam")
 clock = pygame.time.Clock()
